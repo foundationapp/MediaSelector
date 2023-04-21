316e0e85d16b9ca8e6db739f55d0811dedbe246f
Tony Lea <tnylea@gmail.com>
2023-04-21 15:40:18 -0400

    first commit

---
+++ MediaSelector.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\PowerUps\Components\MediaSelector;
+
+use Livewire\Component;
+
+class MediaSelector extends Component
+{
+    public function render()
+    {
+        return view('powerups::mediaselector.media-selector');
+    }
+}
